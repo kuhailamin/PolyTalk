8cc9cede137f9247cd6446a754d23d33f524df66
Batiseba Tekle Yohanes <70596018+batsieba@users.noreply.github.com>
2024-08-03 20:41:04 +0400

    Update main.jsx

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Create a root for React to render the App component into
 ReactDOM.createRoot(document.getElementById('root')).render(
+    // Wrap the App component in React.StrictMode to highlight potential problems
   <React.StrictMode>
     <App />
   </React.StrictMode>,
