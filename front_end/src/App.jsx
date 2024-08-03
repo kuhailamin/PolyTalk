a0d1893bb42a83249849af20c5cfc6ddd2cb73df
Batiseba Tekle Yohanes <70596018+batsieba@users.noreply.github.com>
2024-08-03 20:36:17 +0400

    Update App.jsx

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -6,7 +6,10 @@ import socketIO from "socket.io-client";
 import { api } from "./api";
 import { BASE_URL } from "./api";
 
+// Establish a connection to the Socket.IO server
 const socket = socketIO.connect(BASE_URL);
+
+// Prevent the user from closing the browser window without confirmation
 window.onbeforeunload = function () {
   return false;
 };
