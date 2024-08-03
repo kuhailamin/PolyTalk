94ab27758fdece36b62084004b9507e55005c56a
Batiseba Tekle Yohanes <70596018+batsieba@users.noreply.github.com>
2024-08-03 20:21:28 +0400

    Documentation config.js

--- back_end/config.js
+++ back_end/config.js
@@ -1,12 +1,23 @@
 const OpenAI = require("openai");
 const prompt = require("./model/content");
 
+// Initialize OpenAI with API key from environment variables
 const openai = new OpenAI({
   apiKey: process.env.API_KEY,
 });
 
+/**
+ * Asks a question to the GPT model using OpenAI's API.
+ *
+ * @param {Array} input - The array of messages for the chat session.
+ * @param {string} prompt - The initial prompt to set the context for the GPT model.
+ * @returns {Promise<string>} - The response from the GPT model.
+ */
+
 async function askGPT(input, prompt) {
   // const { text } = await prompt.findOne();
+
+  //Creat a completion request to OpentAI's GPT model
   const { choices } = await openai.chat.completions.create({
     messages: [
       {
@@ -24,4 +35,5 @@ async function askGPT(input, prompt) {
   return content;
 }
 
+// Export the askGPT function for use in other modules
 exports.askGPT = askGPT;
